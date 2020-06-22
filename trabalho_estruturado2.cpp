cc67495b60eb2e13af78b4801778f6fe96db8706
fred1895 <frederico.nascimento97@gmail.com>
2020-06-21 23:53:52 -0300

    Initial commit

---
+++ trabalho_estruturado2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ptb");
+	
+	float carInitialPrice = 0;
+	int option1Chosen = 0, option2Chosen = 0, option3Chosen = 0, option4Chosen = 0, option5Chosen = 0;
+		
+	char answer = 's';
+	
+	cout << "Informe o valor de produção do carro: ";
+	cin >> carInitialPrice;
+	
+	cout <<"\n Opcionais: ";
+	cout <<"\n 1 - Básico: ";
+	cout <<"\n 2 - Trio elétrico (alarme, vidro elétrico e tranca elétrica)";
+	cout <<"\n 3 - Ar condicionado";
+	cout <<"\n 4 - Direção hidráulica";
+	cout <<"\n 5 - Completo (trio elétrico, ar condicionado e direção hidráulica) \n";
+
+	do {
+		int option;
+		cout << "\nDigite a opçaõ que você deseja escolher: ";
+		cin >> option;
+		
+		while (option < 1 || option > 5){
+			cout << "Digite a opçaõ correta (de 1 a 5) que você deseja escolher: ";
+			cin >> option;
+		}
+		
+		float
+		IPI = 8.0,
+		additionPorcentage = 2.0,
+		discountPorcentage = 3.5,
+		carFinalPrice = carInitialPrice, 
+		IPI_CALCULATED = carInitialPrice*IPI/100,
+		additionPrice = carInitialPrice*additionPorcentage/100,
+		additionPriceWithIPI = carFinalPrice*additionPorcentage/100,
+		discountCompleteCar = carFinalPrice*discountPorcentage/100;
+		
+		switch (option){
+			case 1:
+				carFinalPrice += IPI_CALCULATED;
+				option1Chosen ++;
+				break;
+				
+			case 2:
+				carFinalPrice += IPI_CALCULATED;
+				carFinalPrice += additionPriceWithIPI;
+				option2Chosen ++;
+				break;
+				
+			case 3:
+				carFinalPrice += additionPrice + IPI_CALCULATED;
+				option3Chosen ++;
+				break;
+				
+			case 4:
+				carFinalPrice += additionPrice + IPI_CALCULATED;
+				option4Chosen ++;
+				break;
+				
+			case 5:
+				carFinalPrice += IPI_CALCULATED + (additionPrice*3);
+				carFinalPrice -= discountCompleteCar;
+				option5Chosen ++;
+				break;
+				
+			default:
+				cout <<"Operação inválida.";
+		}
+		printf( "O valor total do carro foi %.2f \n", carFinalPrice);
+		//cout <<"O valor total do carro ficou em: " <<carFinalPrice;
+		
+		cout <<"\nDeseja repetir a operação? (s/n) ";
+		cin >> answer;
+		
+	} while (answer == 's' || answer == 'S');
+	
+	cout << "\nA opção 1 foi escolhida " << option1Chosen << " vez(es).";
+	cout << "\nA opção 2 foi escolhida " << option2Chosen << " vez(es).";
+	cout << "\nA opção 3 foi escolhida " << option3Chosen << " vez(es).";
+	cout << "\nA opção 4 foi escolhida " << option4Chosen << " vez(es).";
+	cout << "\nA opção 5 foi escolhida " << option5Chosen << " vez(es).";
+	
+	return 0;
+}
