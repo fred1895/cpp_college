a5df5d8467036b221ebb17638a0da8d34b5f32b3
Fred Almeida <fredericonascimento97@gmail.com>
2020-10-30 16:32:42 -0300

    Adicionando fila

---
+++ lista/fila.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <locale>
+#include <string>
+
+#define MAX_TAM 10
+
+struct queue 
+{
+	float fill[MAX_TAM];
+	int inicio, fim;
+}
+
+void enfileira(queue& fill);
+void desenfileira(queue& fill)
+void elemPrimeiro(queue& fill);
+void situacaoFila(queue& fill);
+
+int main() {
+	
+}
