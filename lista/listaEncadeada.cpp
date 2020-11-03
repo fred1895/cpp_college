0d949ccbf864207d9de25657f68574bcfeb26308
Fred Almeida <fredericonascimento97@gmail.com>
2020-11-03 10:13:52 -0300

    Adicionando lista encadeada

---
+++ lista/listaEncadeada.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <locale>
+# define MAX_LISTA 10
+#include "conio.h"
+
+using namespace std;
+
+// Nó ou nodo
+struct nodo 
+{
+	int info;
+	
+	nodo* prox;
+}
+
+int main () {
+	
+}
