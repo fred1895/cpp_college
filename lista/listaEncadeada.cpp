cb293099e88bffad7f320a92ed6a4f31396dec5a
Fred Almeida <fredericonascimento97@gmail.com>
2020-11-03 15:22:17 -0300

    Adicionando lista encadeada ( metodo insere )

--- lista/listaEncadeada.cpp
+++ lista/listaEncadeada.cpp
@@ -31,3 +31,50 @@ void libera(nodo* ponteiro);
 int main () {
 	
 }
+
+nodo* insereFrente(node* ponteiro, int valor)
+{
+	node* temp = new nodo;
+	if (!temp)
+	{
+		cout << "\nNao foi possivel fazer a alocacao de memoria.\n";
+	}
+	temp->info = valor;
+	temp ->prox = ponteiro;
+	return temp;
+}
+
+nodo* insereFim(node* ponteiro, int valor)
+{
+	nodo* novo, * aux;
+	
+	novo = new nodo;	
+	
+	if (!novo)
+	{
+		cout << "\nNao foi possivel fazer a alocacao de memoria.\n";
+	}
+	novo->info = valor;
+	novo->prox = NULL;
+	if (!ponteiro) {
+		ponteiro = novo;
+	}
+	else {
+		aux = ponteiro;
+		while (aux->prox) {
+			aux = aux->prox;
+		}
+	}
+	return ponteiro;
+}
+
+
+
+
+
+
+
+
+
+
+
