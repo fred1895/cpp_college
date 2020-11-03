43f9caae85dccbe0821d62016d5c7ab1f972ed60
Fred Almeida <fredericonascimento97@gmail.com>
2020-11-03 11:12:38 -0300

    Adicionando lista encadeada

--- lista/listaEncadeada.cpp
+++ lista/listaEncadeada.cpp
@@ -13,6 +13,21 @@ struct nodo
 	nodo* prox;
 }
 
+nodo* insereFrente(node* ponteiro, int valor);
+nodo* insereFim(node* ponteiro, int valor);
+
+void exibe(nodo* ponteiro);
+
+void removeFrente(nodo* ponteiro);
+void removeFim(nodo* ponteiro);
+
+int subtituiNo(nodo* ponteiro, int posicao, int novoValor);
+
+int buscaSequencial(nodo* ponteiro, int valor);
+int contaNos(nodo* ponteiro);
+
+void libera(nodo* ponteiro);
+
 int main () {
 	
 }
